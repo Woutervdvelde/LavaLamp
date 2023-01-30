f54010c8c23354e8a289c9aad527353cb14273b4
Wouter van der Velde <woutervandervelde2000@hotmail.com>
2023-01-30 19:21:44 +0100

    added control menu for lavalamp controls

--- app.js
+++ app.js
@@ -49,3 +49,32 @@ exampleSlider.oninput = (e) => {
     ball1.style.left = `${e.target.value}%`;
     ball2.style.right = `${e.target.value}%`;
 }
+
+const menu = document.getElementById('menu-bar');
+menu.onclick = () => {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+const addBall = document.getElementById('spawn_ball');
+addBall.onclick = () => {
+    lavaLamp.addBall(random(0, canvas.width), random(0, canvas.height), random());
+}
+
+const removeBallButton = document.getElementById('remove_ball');
+removeBallButton.onclick = () => {
+    lavaLamp.balls.pop();
+}
+
+const updateSpeed = (addition) => {
+    LAVA_SPEED = Math.round((LAVA_SPEED + addition) * 10) / 10;
+    if (LAVA_SPEED < 0) LAVA_SPEED = 0;
+    document.getElementById('current_speed').innerHTML = LAVA_SPEED;
+}
+
+const speedDown = document.getElementById('speed_down');
+const speedUp = document.getElementById('speed_up');
+speedDown.onclick = () => updateSpeed(-0.1);
+speedUp.onclick = () => updateSpeed(0.1);
+updateSpeed(0);
